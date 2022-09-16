4ed2047537eaa728696251416958046ac10330bf
joaofp <jvfpdaemoon1@gmail.com>
2022-09-16 00:05:49 -0300

    deploy: finished;

--- src/index.js
+++ src/index.js
@@ -6,7 +6,17 @@ import { authRouter } from './routes/auth.routes.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get("/");
+app.get('/', (req, res, next) => {
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            name: 'name of your app',
+            version: '0.1.0'
+        }
+    });
+
+});
 
 app.use(authRouter);
 
