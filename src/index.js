02d147b0182b272cff27693d3d785f21e548556a
joaofp <jvfpdaemoon1@gmail.com>
2022-09-16 13:46:52 -0300

    deploy

--- src/index.js
+++ src/index.js
@@ -6,11 +6,6 @@ import { authRouter } from './routes/auth.routes.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 app.get('/', (req, res, next) => {
 
