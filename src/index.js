a6a34e67b6c0953c5b94b04bb8c59bb0f342fbf9
joaofp <jvfpdaemoon1@gmail.com>
2022-09-16 13:28:59 -0300

    front: deployed;

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import { authRouter } from './routes/auth.routes.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req, res, next) => {
 
     res.status(200).json({
