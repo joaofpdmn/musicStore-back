79950837584d0faf3159b0b52762a5f022cb5fab
YessicaStern <yessica_stern@hotmail.com>
2022-09-18 10:10:00 -0300

    consertando errinhos back

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { loginAccount, signUpAccount } from '../controllers/auth.controller.js';
-import { list, listSearch } from '../controllers/list.controller.js';
+import { list,listSearch } from '../controllers/list.controller.js';
 
 const authRouter = express.Router();
 
