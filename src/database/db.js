c6cc4dcb5f20d26c0d86865d79dfdb4e5dd8ca47
joaofp <jvfpdaemoon1@gmail.com>
2022-09-19 12:39:32 -0300

    corrigindo

--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+const mongoClient = new MongoClient("mongodb+srv://driven:HN08w9IgV9AJ1Zle@musicstorecluster.ykv1vl7.mongodb.net/?retryWrites=true");
 
 try {
     await mongoClient.connect();
